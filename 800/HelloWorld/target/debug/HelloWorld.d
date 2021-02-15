8693308eb7b313280f232b1958a01dcadcf498c9
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-15 10:01:46 +0100

    env setup

---
+++ 800/HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+C:\Users\jak0024\source\repos\CodeForcesRUST\CodeForcesRUST\800\HelloWorld\target\debug\HelloWorld.exe: C:\Users\jak0024\source\repos\CodeForcesRUST\CodeForcesRUST\800\HelloWorld\src\main.rs
---
+++ 800/HelloWorld/target/debug/deps/HelloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\jak0024\source\repos\CodeForcesRUST\CodeForcesRUST\800\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
+
+C:\Users\jak0024\source\repos\CodeForcesRUST\CodeForcesRUST\800\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
+
+src\main.rs:
